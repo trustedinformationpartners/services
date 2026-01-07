2b76036fe6714e0042f0e13b1b135bf228618613
lvanderpeet <162470345+lvanderpeet@users.noreply.github.com>
2026-01-07 12:03:40 +0100

    version to 1.0

--- tools/build.js
+++ tools/build.js
@@ -7,7 +7,7 @@ const DOCS = "docs"
 const OUT = "build"
 const TITLE = "Generic services for digital infrastructure"
 const BOOK = "generic-services"
-const VERSION = "0.1"
+const VERSION = "1.0"
 
 let date = new Date().toISOString().substr(0, 10)
 
